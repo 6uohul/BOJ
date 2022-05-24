ef60051b982bd21f7c33a41120ff6ac5b4a9efa7
6uohul <kiy010604@naver.com>
2022-05-24 19:31:12 +0900

    2022.05.24 BOJ_11047.cpp

---
+++ 11047.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int a[11];
+int result = 0;
+
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = n - 1; i >= 0; i--)
+    {
+        result = k / a[i];
+        k %= a[i];
+    }
+
+    cout << result;
+}
